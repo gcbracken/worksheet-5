b31113943010635c79e5b7c4c39d5a5eaf97c5b2
gcbracken <gbracken@nebrwesleyan.edu>
2014-10-07 09:56:05 -0500

    Problems 1&2 updated

--- index.js
+++ index.js
@@ -2,3 +2,36 @@
 /*jslint browser: true*/ // This line tells jsLint that the code will run in a browser.
 /*global console*/ // This line tells jsLint that you're allowed to use console.log for printing.
 
+//Problem 1
+var coinFlip = function () {
+  var number = Math.random();
+  if (number < 0.5) {
+    return ("Heads");
+  }else{
+    return ("Tails");
+  };
+};
+
+coinFlip()
+
+//Problem 2
+var diceRoll = function () {
+  var number = Math.random();
+  if (number >= 0.83) {
+    return (6);
+  }else if (0.83 > number && number >= 0.67) {
+          return (5);
+        };
+  if (0.67 > number && number >= 0.5) {
+    return (4);
+  }else if (0.5 > number && number >= 0.33) {
+          return (3);
+        };
+  if (0.33 > number && number >= 0.167) {
+    return (2);
+  }else if (number < 0.167) {
+          return (1);
+  };
+};
+
+diceRoll()
